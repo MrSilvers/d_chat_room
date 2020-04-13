975f72d82ef41bd3f570e0c63f19577ef45cc598
MrSilvers <1150542091@qq.com>
2020-04-14 00:07:58 +0800

    realize multiple client

--- d_chat/views.py
+++ d_chat/views.py
@@ -10,33 +10,34 @@ from .models import *
 send_flag = 0
 request_list = []
 user_id_list = []
+request_dict = {}
 
 @accept_websocket
 @login_required
 def chat(request):
+    user_id = request.session.get("user_id")
+    print(user_id)
     if request.is_websocket:
-        if request.session.get('user_id') in user_id_list:
-            pass
-        else:
-            request_list.append(request)
+        if not request_dict.get(user_id):
+            request_dict[user_id] = request
+        print(request_dict,request_dict.values())
         while True:
             time.sleep(1)
             try:
                 data = request.websocket.wait()  # 接受前端发送来的数据
                 if data:
                     print(data)
-                    if data != '886':
-                        receive_data = data
-                        if receive_data:
-                            for item in request_list:
-                                item.websocket.send(receive_data)  # 发送给前端的数据
-                    else:
-                        message = {"user_id":"root","message":"connect close."}
-                        request.websocket.send(json.dumps(message))
+                    data = json.loads(str(data, encoding = "utf-8"))
+                    print(data)
+                    send_message(data,request_dict.values())
+                    if data.get("message") == "886":
+                        message = {"user_id":user_id,"message":"connect closed."}
+                        send_message(message,request_dict.values())
                         request.websocket.close()
-                        # remove current in user_id_list and requests_list
-
+                        # remove current request in request_dict
+                        request_dict.pop(user_id)
             except Exception as e:
+                print(e)
                 request.websocket.close()
                 print('socket请求关闭！！！')
 
@@ -47,6 +48,10 @@ def chat_panel(request):
     elif request.method == "POST":
         username = request.session['user_id']
         receiver = request.POST.get('receiver')
+
+def send_message(data,send_to_list):
+    for item in send_to_list:
+        item.websocket.send(json.dumps(data))  # 发送给前端的数据
 
 
 
